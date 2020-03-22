3541620b294ad4c0c06618be7b5e3536c73beddf
dev-hjJoo <dodam.janna1997@gmail.com>
2020-03-22 22:44:40 +0900

    upload files of myWebBrowser

---
+++ SECTION1/UNIT2/1-MyWebBrowser/1-MyWebBrowser/AppDelegate.swift
@@ -0,0 +1,54 @@
+//
+//  AppDelegate.swift
+//  1-MyWebBrowser
+//
+//  Created by hyovely on 2020/03/21.
+//  Copyright © 2020 mobileX.CWNU. All rights reserved.
+//
+
+import UIKit
+
+// 마지막 페이지 주소를 UserDefaults에서 관리하기 위한 키 값
+let lastPageURLDefaultKey: String = "lastURL"
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    // MARK: - Properties
+    var lastPageURL: URL?
+    
+    // MARK: - Methods
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        self.lastPageURL = UserDefaults.standard.url(forKey: lastPageURLDefaultKey)
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        
+        let userDefaults: UserDefaults = UserDefaults.standard
+        
+        userDefaults.set(self.lastPageURL, forKey: lastPageURLDefaultKey)
+        userDefaults.synchronize()
+    }
+    
+    
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ SECTION1/UNIT2/1-MyWebBrowser/1-MyWebBrowser/ViewController.swift
@@ -0,0 +1,121 @@
+//
+//  ViewController.swift
+//  1-MyWebBrowser
+//
+//  Created by hyovely on 2020/03/21.
+//  Copyright © 2020 mobileX.CWNU. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    // MARK: IBOutlets
+    @IBOutlet var webView: WKWebView!
+    @IBOutlet var activitityIndicator: UIActivityIndicatorView!
+    
+    // MARK: - Methods
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.webView.navigationDelegate = self
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let firstPageURL: URL?
+        
+        if let lastURL: URL = UserDefaults.standard.url(forKey: lastPageURLDefaultKey) {
+            firstPageURL = lastURL
+        } else {
+            firstPageURL = URL(string: "https://www.google.com")
+        }
+        
+        guard let pageURL: URL = firstPageURL else {
+            return
+        }
+        
+        let urlRequest: URLRequest = URLRequest(url: pageURL)
+        self.webView.load(urlRequest)
+    }
+    
+    // MARK: IBActions
+    @IBAction func goBack(_ sender: UIBarButtonItem) {
+        self.webView.goBack()
+    }
+    
+    @IBAction func goForward(_ sender: UIBarButtonItem) {
+        self.webView.goForward()
+    }
+    
+    @IBAction func refresh(_ sender: UIBarButtonItem) {
+        self.webView.reload()
+    }
+    
+    // MARK: Custom Methods
+    func showNetworkingIndicators() {
+        self.activitityIndicator.isHidden = false
+        self.activitityIndicator.startAnimating()
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func hideNetworkingIndicators() {
+        self.activitityIndicator.isHidden = true
+        self.activitityIndicator.stopAnimating()
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+}
+
+
+extension ViewController: WKNavigationDelegate {
+    
+    // MARK: WKNavigationDelegate
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("did finish navigation")
+        
+        if let appDelegate: AppDelegate = UIApplication.shared.delegate as? AppDelegate {
+            appDelegate.lastPageURL = webView.url
+        }
+        
+        webView.evaluateJavaScript("document.title") { (value: Any?, error: Error?) in
+            if let error: Error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let title: String = value as? String else {
+                return
+            }
+            
+            self.navigationItem.title = title
+        }
+        self.hideNetworkingIndicators()
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        print("did fail navigation")
+        print("\(error.localizedDescription)")
+        
+        self.hideNetworkingIndicators()
+        let message: String = "오류발생\n" + error.localizedDescription
+        
+        let alert: UIAlertController
+        alert = UIAlertController(title: "알림", message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        let okayAction: UIAlertAction
+        okayAction = UIAlertAction(title: "확인", style: UIAlertAction.Style.cancel, handler: nil)
+        
+        alert.addAction(okayAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation) {
+        print("did start navigation")
+        self.showNetworkingIndicators()
+    }
+}
